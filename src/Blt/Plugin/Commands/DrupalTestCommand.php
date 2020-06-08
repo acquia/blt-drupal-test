90fe7cdf84f70f55f157f810a87870ab6406f739
Dane Powell <git@danepowell.com>
2020-06-08 16:51:58 -0700

    Update DrupalTestCommand.php

--- src/Blt/Plugin/Commands/DrupalTestCommand.php
+++ src/Blt/Plugin/Commands/DrupalTestCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acquia\Blt\Robo\Commands\Tests;
+namespace Acquia\BltDrupalTest\Blt\Plugin\Commands;
 
 use Acquia\Blt\Robo\Exceptions\BltException;
 use Robo\Contract\VerbosityThresholdInterface;
